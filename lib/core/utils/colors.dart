b3da3a7d6d1bc80e0ab8d191f843220c55bedb44
Devang <devangdhameliya5@gmail.com>
2025-09-18 16:14:24 +0530

    Integrate Firebase support with configuration updates and plugin registrations

---
+++ lib/core/utils/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color appBarColor = Color(0xFFf0f4f7);
+const Color bodyColor = Color(0xFFfcfefd);
+const Color iconBackgroundColor = Color(0xFF34353d);
+const Color bottomSheetColor = Color(0xFF222222);
